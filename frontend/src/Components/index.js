d41d3adf9d09339ae567799dc3e6f349b3dc3649
Rudra <goutam.prasad.2707@gmail.com>
2025-04-10 23:47:37 +0530

    add Title component

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Button from "./Button";
+import Title from "./Title";
 
-export { Navbar, Footer, Button };
+export { Navbar, Footer, Button, Title };
