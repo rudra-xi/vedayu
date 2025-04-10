feb6514a30a14084323c7196584cc652b8431929
Rudra <goutam.prasad.2707@gmail.com>
2025-04-10 23:46:52 +0530

    refactor Button component styles and add Title component for improved UI

--- frontend/src/Components/Button.jsx
+++ frontend/src/Components/Button.jsx
@@ -3,9 +3,12 @@ import React from "react";
 function Button({ buttonText, onClick, disabled = false }) {
 	return (
 		<button
-			className={`px-7 h-10 bg-accent border-2 border-tertiary rounded-full cursor-pointer
-        ${disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-accent/20"}
-        transition-colors duration-200`}
+			className={`px-7 h-10 bg-accent border-2 border-primary rounded-full cursor-pointer transition-all duration-500 ease-in-btn
+        ${
+			disabled
+				? "opacity-50 cursor-not-allowed"
+				: "hover:bg-accent/80 hover:scale-90 "
+		}`}
 			onClick={onClick}
 			disabled={disabled}
 		>
---
+++ frontend/src/Components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title({ Title }) {
+	return (
+		<div className="font-inf text-6xl font-bold text-primary text-center pb-20">
+			{Title}.
+		</div>
+	);
+}
+
+export default Title;
