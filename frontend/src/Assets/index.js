e0777140d836bd372e6aa9f557a1fa14e45dc12b
Rudra <goutam.prasad.2707@gmail.com>
2025-04-10 15:59:48 +0530

    add logo assets and update component exports for authentication

---
+++ frontend/src/Assets/index.js
@@ -0,0 +1,4 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export { logoDark, logoLight };
--- frontend/src/Pages/index.js
+++ frontend/src/Pages/index.js
@@ -1,8 +1,8 @@
 import Dashboard from "./Dashboard";
-import Sign from "./Sign";
+import Login from "./Login";
 import Landing from "./Landing";
 import Insights from "./Insights";
 import Profile from "./Profile";
 import Heart from "./Heart";
 
-export { Dashboard, Sign, Landing, Insights, Profile, Heart };
+export { Dashboard, Login, Landing, Insights, Profile, Heart };
