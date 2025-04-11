b9eeb2b7bfba1c5730f94fbf3cc2a2142738d35e
Rudra <goutam.prasad.2707@gmail.com>
2025-04-11 22:17:45 +0530

    refactor: optimize Button component's transition duration and hover effects

--- frontend/src/Components/Button.jsx
+++ frontend/src/Components/Button.jsx
@@ -3,12 +3,11 @@ import React from "react";
 function Button({ buttonText, onClick, disabled = false }) {
 	return (
 		<button
-			className={`px-7 h-10 bg-accent border-2 border-primary rounded-full cursor-pointer transition-all duration-500 ease-in-btn
-        ${
-			disabled
-				? "opacity-50 cursor-not-allowed"
-				: "hover:bg-accent/80 hover:scale-90 "
-		}`}
+			className={`px-7 h-10 bg-accent border-2 border-primary rounded-full cursor-pointer transition-all duration-300 ease-custom ${
+				disabled
+					? "opacity-50 cursor-not-allowed"
+					: "hover:scale-95 "
+			}`}
 			onClick={onClick}
 			disabled={disabled}
 		>
